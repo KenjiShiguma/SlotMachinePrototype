fd3ad9aa8e1109de4e2e206cc2bc82cbdc6a0f07
Kermit Mitchell III <kenjishiguma@gmail.com>
2020-03-19 00:51:22 -0400

    Added Score for First Row and Award Reference Panel UI
    Added an EvaluateBoard() function to determine the score output based on predetermine permutations of panels. The score text now updates based on the first row's results. Left a Reference Panel to show score and award multipler, to do in next commit.

---
+++ Assets/Resources/_Scripts/Board.cs
@@ -0,0 +1,179 @@
+﻿// Author: Kermit Mitchell III
+// Start Date: 03/17/2020 8:45 PM | Last Edited: 03/19/2020 12:45 AM
+// This script runs the game board and creates new spins and etc.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    // Declare the variables
+
+    // TODO: Make a singleton of the board for use in a game manager script
+    private Slot[] slots; // the slots on the Board, from left to right 0-4
+
+    [SerializeField] private int spinCounter; // number of spins player has
+    private Text spinText; // the text for the spinCounter
+
+    [SerializeField] private int score; // number of points player has
+    private Text scoreText; // the text for the score
+
+    // Initalize the variables
+    private void Start()
+    {
+        // Create the spin counter
+        spinCounter = 999;
+        spinText = GameObject.Find("SpinText").GetComponent<Text>();
+        spinText.text = "Spins: " + spinCounter.ToString("D3");
+
+        score = 0;
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = "Score: " + score.ToString("D7");
+
+        // Create the GameBoard
+        slots = new Slot[5]; // because the GameBoard is a 4x5
+        for(int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = this.transform.Find("Slot (" + i + ")").GetComponent<Slot>();
+        }
+
+    }
+
+    // Generates a new game board, deducts a spin
+    public void Spin()
+    {
+        Debug.Log("Time: " + Time.time + " Attempting to spin..." + " | " + "spinCounter: " + spinCounter);
+
+        if (spinCounter < 1)
+        {
+            // Error
+            Debug.LogError("Time: " + Time.time + " Error: " + "You need at least one Spin to Spin " +
+                " | " + "spinCounter: " + spinCounter);
+            return;
+        }
+        else
+        {
+            spinCounter--;
+            spinText.text = "Spins: " + spinCounter.ToString("D3");
+            // Spin each Panel on the Board
+            foreach(Slot slot in slots)
+            {
+                foreach(Panel panel in slot.getPanels())
+                {
+                    panel.SpinPanel();
+                }
+            }
+
+            // Evaluate the board for the pay table
+            EvaluateBoard();
+            
+        }
+    }
+
+    // Determines the payout based on predefined pay tables/permutations of the icons
+    private void EvaluateBoard()
+    { 
+        int slotIndex = slots.Length - 1; // current slotIndex; starts at top right, moves left
+        int panelIndex = 0; // current panelIndex; starts at top, moves down
+        // panelIndedx will remain 0 for the first row's pay table
+
+        PanelIcon referencePanel = 0; // the icon of reference panel we are comparing to
+        PanelIcon nextPanel = 0; // the icon of each subsequent neighboring icon to compare
+
+        int localMaxOccurance = 1; // the current largest combination of the same icon
+        int localMaxIndex = 0; // where the index of the currentMaxOccurance starts
+
+        // For the absMax found in this search
+        PanelIcon absMaxPanel = 0; // the icon associated with absMaxOccurance
+        int absMaxOccurance = 1; // the highest recorded combination of the same icon
+        int absMaxIndex = 0; // where the index of the absMaxOcc starts
+
+        // Horizontal Row 1 // TODO: Horizontal Rows 2, 3, 4, V shape, W shape
+        // Start at the right-most panel in this row 
+        referencePanel = slots[slotIndex].getPanels()[panelIndex].GetFruit();
+        localMaxIndex = slotIndex;
+        //Debug.LogWarning("Time: " + Time.time + " | " + "ReferencePanel: " + referencePanel + " | " +
+        //                "slotIndex: " + slotIndex + " | " + "localMaxIndex: " + localMaxIndex);
+        slotIndex--;
+        nextPanel = slots[slotIndex].getPanels()[panelIndex].GetFruit();
+        //Debug.Log("Time: " + Time.time + " | " + "NextPanel: " + nextPanel + " | " +
+        //           "slotIndex: " + slotIndex);
+
+        while (slotIndex >= -1)
+        { 
+            // Keep track of the same occurances
+            if (nextPanel == referencePanel/* && !((slotIndex + 1) < absMaxOccurance) && !()*/)
+            {
+                //Debug.Log("Time: " + Time.time + " | " + "Same icon found! Continuing search...");
+                localMaxOccurance++;
+            }
+            // The chain is broken
+            else
+            {
+                //Debug.Log("Time: " + Time.time + " | " + "Local chain broken..." + " | " +
+                //        "LocalMaxOcc: " + localMaxOccurance + " | " +
+                //        "AbsMaxOcc (BEFORE): " + absMaxOccurance);
+
+                // Update the max occurance if needed
+                if (localMaxOccurance > absMaxOccurance && Panel.panelScores[referencePanel] > Panel.panelScores[absMaxPanel])
+                {
+                    absMaxOccurance = localMaxOccurance;
+                    absMaxPanel = referencePanel;
+                    absMaxIndex = localMaxIndex;
+
+                    //Debug.Log("Time: " + Time.time + " | " + "Updating AbsMax..." + " | " +
+                    //    "AbsMaxOcc: " + absMaxOccurance + " | " + "AbsMaxIndex: " + absMaxIndex);
+                }
+
+                // Stop the search if you can't best the current absMaxOcc
+                if ((slotIndex + 1) < absMaxOccurance)
+                {
+                    break;
+                }
+                // Otherwise, try the next panel to the left and reset the occurance counters
+                else
+                {
+                    referencePanel = nextPanel;
+                    localMaxOccurance = 1;
+                    localMaxIndex = slotIndex;
+                    //Debug.LogWarning("Time: " + Time.time + " | " + "ReferencePanel: " + referencePanel + " | " +
+                    //    "slotIndex: " + slotIndex + " | " + "localMaxIndex: " + localMaxIndex);
+
+                }
+            }
+
+            slotIndex--;
+            if(slotIndex < 0)
+            {
+                nextPanel = PanelIcon.Default;
+            }
+            else
+            {
+                nextPanel = slots[slotIndex].getPanels()[panelIndex].GetFruit();
+            }
+
+            //Debug.Log("Time: " + Time.time + " | " + "NextPanel: " + nextPanel + " | " +
+            //          "slotIndex: " + slotIndex);
+        }
+
+        // Report the max occurance and pay the player if needed
+
+        int pointsGained = (Panel.panelScores[absMaxPanel] * absMaxOccurance);
+        Debug.Log("Time: " + Time.time + "| " + "Icon: " + absMaxPanel + " | " + 
+            "AbsMaxOcc: " + absMaxOccurance + " | " + "AbsMaxIndex: " + absMaxIndex + " | " +
+            "Points: " + pointsGained + " (" + Panel.panelScores[absMaxPanel] + "*" + absMaxOccurance + ")");
+
+        // TODO: Link up points gained and reference panel to the reference panel UI
+        // Update Score UI // TODO: Make this a EventListner triggered UI update thing.
+        score += pointsGained;
+        scoreText.text = "Score: " + score.ToString("D7");
+
+    }
+
+
+
+
+}
--- Assets/Resources/_Scripts/Panel.cs
+++ Assets/Resources/_Scripts/Panel.cs
@@ -1,5 +1,5 @@
 ﻿// Author: Kermit Mitchell III
-// Start Date: 03/17/2020 9:30 PM | Last Edited: 03/17/2020 11:10 PM
+// Start Date: 03/17/2020 9:30 PM | Last Edited: 03/18/2020 8:15 PM
 // This script helps modify Panels
 
 using System.Collections;
@@ -16,7 +16,7 @@ public class Panel : MonoBehaviour
     private SpriteRenderer icon; // a reference to Sprite component | the fruit image on the panel
     private int points; // the points gained by this fruit
     public static Dictionary<PanelIcon, Sprite> panelSprites; // Holds all panel Sprites // TODO: Make this a Singleton
-    public static Dictionary<PanelIcon, int> fruitScores; // Holds all fruit scores // TODO: Make this a Singleton
+    public static Dictionary<PanelIcon, int> panelScores; // Holds all fruit scores // TODO: Make this a Singleton
 
     private void Start()
     {
@@ -36,18 +36,18 @@ public class Panel : MonoBehaviour
         }
 
         // Create the fruitScores dictionary
-        if (fruitScores == null)
+        if (panelScores == null)
         {
-            fruitScores = new Dictionary<PanelIcon, int>();
-            fruitScores.Add(PanelIcon.Default, 0);
-            fruitScores.Add(PanelIcon.Apple, 100);
-            fruitScores.Add(PanelIcon.Kiwi, 100);
-            fruitScores.Add(PanelIcon.Lemon, 100);
-            fruitScores.Add(PanelIcon.Orange, 100);
-            fruitScores.Add(PanelIcon.Peach, 100);
-            fruitScores.Add(PanelIcon.Pear, 100);
-            fruitScores.Add(PanelIcon.Pomegranate, 100);
-            fruitScores.Add(PanelIcon.Watermelon, 100);
+            panelScores = new Dictionary<PanelIcon, int>();
+            panelScores.Add(PanelIcon.Default, 0);
+            panelScores.Add(PanelIcon.Apple, 100);
+            panelScores.Add(PanelIcon.Kiwi, 100);
+            panelScores.Add(PanelIcon.Lemon, 100);
+            panelScores.Add(PanelIcon.Orange, 100);
+            panelScores.Add(PanelIcon.Peach, 100);
+            panelScores.Add(PanelIcon.Pear, 100);
+            panelScores.Add(PanelIcon.Pomegranate, 100);
+            panelScores.Add(PanelIcon.Watermelon, 100);
         }
 
         // Grab the sprite reference and change icon to match the fruit
@@ -59,23 +59,33 @@ public class Panel : MonoBehaviour
     }
 
     // TODO: Make an event listener if the fruit value changes dynamically to call this function
-    // Sets the fruit and updates the image and value
-    public void SetFruit(PanelIcon icon)
-    {
-        fruit = icon;
-        this.icon.sprite = panelSprites[fruit]; // Sets the sprite of the fruit
-        points = fruitScores[fruit]; // Set the value of the fruit
-
-    }
+    
 
     // Randomly chooses a fruit for the panel, 1-8 or 1-(how many fruit are in the game)
     public void SpinPanel()
     {
         int randFruit = UnityEngine.Random.Range(1, Enum.GetNames(typeof(PanelIcon)).Length);
-        Debug.Log("Time: " + Time.time + " | " + "Spinning this panel..." + " | " +
+        /*Debug.Log("Time: " + Time.time + " | " + "Spinning this panel..." + " | " +
             "CurrentFruit: " + fruit + " | " + "Rolled Fruit: " + (PanelIcon)randFruit + 
-            "(" + randFruit + ")");
+            "(" + randFruit + ")");*/
         SetFruit((PanelIcon)randFruit);
+    }
+
+    // Setters
+
+    // Sets the fruit and updates the image and value
+    public void SetFruit(PanelIcon icon)
+    {
+        fruit = icon;
+        this.icon.sprite = panelSprites[fruit]; // Sets the sprite of the fruit
+        points = panelScores[fruit]; // Set the value of the fruit
+
+    }
+
+    // Getters
+    public PanelIcon GetFruit()
+    {
+        return this.fruit;
     }
 
 }
